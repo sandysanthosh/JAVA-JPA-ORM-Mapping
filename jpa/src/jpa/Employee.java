ee454d1648ebb7bdd3ba5e3265bd9af2ea0f719c
santhosh kumar <sandysanthosh101@gmail.com>
2018-01-13 00:39:14 +0530

    Add files via upload

---
+++ jpa/src/jpa/Employee.java
@@ -0,0 +1,53 @@
+package jpa;	
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Employee {
+	@Id
+	 @GeneratedValue(strategy = GenerationType.AUTO) 
+	 private int eid;
+	   private String ename;
+	   private double salary;
+	   private String deg;
+	   
+		
+	
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getDeg() {
+		return deg;
+	}
+	public void setDeg(String deg) {
+		this.deg = deg;
+	}
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + ", salary="
+				+ salary + ", deg=" + deg + "]";
+	}
+
+	
+}
---
+++ jpa/src/jpa/UpdateEmployee.java
@@ -0,0 +1,26 @@
+package jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class UpdateEmployee {
+   public static void main( String[ ] args ) {
+      EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Eclipselink_JPA" );
+      
+      EntityManager entitymanager = emfactory.createEntityManager( );
+      entitymanager.getTransaction( ).begin( );
+      Employee employee = entitymanager.find( Employee.class, 1201 );
+      
+      //before update
+      System.out.println( employee );
+      employee.setSalary( 46000 );
+      employee.setEname("Sandy");
+      entitymanager.getTransaction( ).commit( );
+      
+      //after update
+      System.out.println( employee );
+      entitymanager.close();
+      emfactory.close();
+   }
+}
